6f9e23e7551ab2e95e186783830e497884364be2
Lavenia Stewart <lavenias123@gmail.com>
2023-03-31 10:42:30 -0700

    first commit Err-APPLICATION FAILED TO START

---
+++ src/main/java/com/promineotech/jeep/dao/jdbcTemplate.java
@@ -0,0 +1,17 @@
+package com.promineotech.jeep.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.promineotech.jeep.entity.Jeep;
+
+public class jdbcTemplate {
+
+	public static List<Jeep> query(String sql, Map<String, Object> params, RowMapper rowMapper) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
